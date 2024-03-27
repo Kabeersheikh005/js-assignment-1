719f2bd1dab992ba02bba9857cde9485dd147021
Kabeersheikh005 <kabeersheikh008@gmail.com>
2024-03-27 22:06:14 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,131 @@
+
+// Declare 5 legal variables names
+
+let $fullName = "muhammad kabeer";
+console.log($fullName);
+let firstName = "muhammad ";
+console.log(firstName);
+let _last_name = " kabeer";
+console.log(_last_name);
+let age = 18;
+console.log(age);
+let batch_name ="batch11" ;
+console.log(batch_name);
+let NAME = "muhammad kabeer";
+console.log(NAME);
+
+// 5 illegal variable names
+
+// let @name = "muhammad kabeer";
+// console.log(@name);
+// let first-name = "muhammad ";
+// console.log(first-name);
+// let 2lastname = " kabeer";
+// console.log(2lastname);
+// let !Age = 18;
+// console.log(!Age);
+// let batch name ="batch11" ;
+// console.log(batch name);
+// let %name 
+
+
+
+// --------TABLE-------------
+
+
+let table1 = ("5 x 1 ="+ 5*1);
+console.log( table1);
+
+let table2 = ("5 x 2 ="+ 5*2);
+console.log( table2);
+
+let table3 = ("5 x 3 ="+ 5*3);
+console.log( table3);
+
+let table4 = ("5 x 4 ="+ 5*4);
+console.log( table4);
+
+let table5 = ("5 x 5 ="+ 5*5);
+console.log( table5);
+
+let table6 = ("5 x 6 ="+ 5*6);
+console.log( table6);
+
+let table7 = ("5 x 7 ="+ 5*7);
+console.log( table7);
+
+let table8 = ("5 x 8 ="+ 5*8);
+console.log( table8);
+
+let table9 = ("5 x 9 ="+ 5*9);
+console.log( table9);
+
+let table10 = ("5 x 10 ="+ 5*10);
+console.log( table10);
+
+
+// -------------------MATH EXPRESSIONS---------------
+
+let num = 10;
+let newNum= ++num;
+console.log(newNum);
+
+
+let num1 = 10;
+let newNum1= ++num;
+let $newNum= num++;
+console.log($newNum);
+
+
+
+
+let num2 = 10;
+let newNum2= --num;
+console.log(newNum);
+
+
+let num3 = 10;
+let newNum3= --num3;
+let $newNum3= ++num3;
+console.log($newNum3);
+
+
+
+
+// ----------------------USER INPUT & CONDITIONAL STATEMENT----------------------------
+
+
+
+let score = prompt("enter your score (0-100):");
+
+ let grade;
+
+
+
+if (score >= 90 && score <= 100) {
+
+ grade = "A";
+
+ } else if (score >= 70 && score <= 89) {
+
+
+
+grade = "B";
+
+ } else if (score >= 60 && score <= 69) {
+
+ grade = "C";
+
+ } else if (score >= 50 && score <= 59) {
+
+ grade = "D";
+
+ } else if (score >= 0 && score <= 49) {
+
+ grade = "F";
+
+}
+
+alert("according to your scores, your grade was: "+ grade);
+console.log("according to your scores, your grade was: "+ grade);
+ 
